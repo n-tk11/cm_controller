8e09c048f7dc1e45131dcbb7cbb00cb8410aa491
n-tk11 <tulsorawit@hotmail.com>
2023-10-28 16:44:47 +0700

    Finish API to run container from docker cli

--- docker.go
+++ docker.go
@@ -1,151 +1,79 @@
 package main
 
 import (
-	"strconv"
-	"log"
-	"context"
 	"fmt"
-	"github.com/docker/docker/client"
-	natting "github.com/docker/go-connections/nat"
-	"github.com/docker/docker/api/types/container"
-	network "github.com/docker/docker/api/types/network"
-	"github.com/docker/docker/api/types"
-	"github.com/opencontainers/image-spec/specs-go/v1"
-	"github.com/docker/docker/api/types/mount"
 	"net"
+	"os"
+	"os/exec"
+	"strconv"
+
+	"github.com/docker/docker/api/types/mount"
 )
 
-var last_daemon_port int = 7877 
+var last_daemon_port int = 7877
 
-func run_container(containername string,imagename string, port string, inputEnv []string, mounts []mount.Mount) error {
-	client, err := client.NewEnvClient()
-	if err != nil {
-		log.Fatalf("Unable to create docker client")
-	}
+func run_container(containerName string, imageName string, portMapping string, inputEnv []string, mounts []mount.Mount) error {
 
-	// Define a PORT opening
-	newport, err := natting.NewPort("tcp", port)
-	if err != nil {
-		fmt.Println("Unable to create docker port")
-		return err
-	}
-
-	new_daemon_port, err := natting.NewPort("tcp", "7878")
-	if err != nil {
-		fmt.Println("Unable to create docker port")
-		return err
-	}
-	host_daemon_port := last_daemon_port+1
+	host_daemon_port := last_daemon_port + 1
 	for isPortInUse(strconv.Itoa(host_daemon_port)) {
 		host_daemon_port += 1
 		//fmt.Println(host_daemon_port)
 	}
-	// Configured hostConfig: 
-	// https://godoc.org/github.com/docker/docker/api/types/container#HostConfig
-	hostConfig := &container.HostConfig{
-		PortBindings: natting.PortMap{
-			newport: []natting.PortBinding{
-				{
-					HostIP:   "0.0.0.0",
-					HostPort: port,
-				},
-			},
-			new_daemon_port: []natting.PortBinding{
-				{
-					HostIP: "0.0.0.0",
-					HostPort: strconv.Itoa(host_daemon_port),
-				},
-			},
-		},
-		RestartPolicy: container.RestartPolicy{
-			Name: "always",
-		},
-		LogConfig: container.LogConfig{
-			Type:   "json-file",
-			Config: map[string]string{},
-		},
-		CapAdd: []string{
-			"cap_sys_ptrace", 
-			"cap_checkpoint_restore"},
-		SecurityOpt: []string{
-			"apparmor=unconfined",
-			"systempaths=unconfined",
-		},
-		Mounts: mounts,
-		/*
-		[]mount.Mount{
-            {
-                Type:   mount.TypeBind,
-                Source: "/mnt/checkpointfs",
-                Target: "/checkpointfs",
-            },
-						{
-							  Type: mount.TypeBind,
-								Source: "/tmp",
-								Target: "/tmp",
-						},
-    },*/
+	// Define variables to hold the command options based on your logic
+	capabilities := []string{"cap_sys_ptrace", "cap_checkpoint_restore"}
+
+	// Create a slice to hold the Docker command and its arguments
+	cmdArgs := []string{
+		"docker", "run",
+		"--name", containerName,
+		"-p", portMapping,
+	}
+	daemon_portMapping := strconv.Itoa(host_daemon_port) + ":7878"
+	cmdArgs = append(cmdArgs, "-p", daemon_portMapping)
+	// Add capabilities to the command arguments
+	for _, cap := range capabilities {
+		cmdArgs = append(cmdArgs, "--cap-add", cap)
 	}
 
-	// Define Network config (why isn't PORT in here...?:
-	// https://godoc.org/github.com/docker/docker/api/types/network#NetworkingConfig
-	networkConfig := &network.NetworkingConfig{
-		EndpointsConfig: map[string]*network.EndpointSettings{},
+	//--security-opt systempaths=unconfined --security-opt apparmor=unconfined
+	securityOpts := []string{"systempaths=unconfined", "apparmor=unconfined"}
+	for _, secOpt := range securityOpts {
+		cmdArgs = append(cmdArgs, "--security-opt", secOpt)
 	}
-	gatewayConfig := &network.EndpointSettings{
-		Gateway: "gatewayname",
-	}
-	networkConfig.EndpointsConfig["bridge"] = gatewayConfig
-
-	// Define ports to be exposed (has to be same as hostconfig.portbindings.newport)
-	exposedPorts := map[natting.Port]struct{}{
-		newport: struct{}{},
-		new_daemon_port: struct{}{},
+	//Add mount arguments
+	for _, mount := range mounts {
+		mountArg := "type=" + string(mount.Type) + ",source=" + mount.Source + ",target=" + mount.Target
+		cmdArgs = append(cmdArgs, "--mount", mountArg)
 	}
 
-	// Configuration 
-	// https://godoc.org/github.com/docker/docker/api/types/container#Config
-	config := &container.Config{
-		Cmd: []string{"ff_daemon"},
-		Image:        imagename,
-		Env: 		  inputEnv,
-		ExposedPorts: exposedPorts,
-		Hostname:     fmt.Sprintf("%s", imagename),
-		AttachStdin: true,
-		//AttachStdout: true,
+	//Add env arguments
+	for _, env := range inputEnv {
+		cmdArgs = append(cmdArgs, "-e", env)
 	}
+	cmdArgs = append(cmdArgs, "-d", "--init")
 
-	platform := &v1.Platform{
-		Architecture: "amd64",
-		OS: "linux",
-	}
+	// Add the image name to the command arguments and command to start with
+	cmdArgs = append(cmdArgs, imageName, "ff_daemon")
 
-	// Creating the actual container. This is "nil,nil,nil" in every example.
-	cont, err := client.ContainerCreate(
-		context.Background(),
-		config,
-		hostConfig,
-		networkConfig,
-		platform,
-		containername,
-	)
+	// Create the command using the constructed arguments
+	cmd := exec.Command(cmdArgs[0], cmdArgs[1:]...)
 
+	// Set the output and error pipes to capture the command's output
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	// Run the Docker CLI command
+	err := cmd.Run()
 	if err != nil {
-		log.Println(err)
+		fmt.Printf("Error running Docker command: %v\n", err)
 		return err
 	}
-
-	// Run the actual container 
-	client.ContainerStart(context.Background(), cont.ID, types.ContainerStartOptions{})
-	log.Printf("Container %s is created", cont.ID)
-
-	service := container_subscribe(containername, imagename,  strconv.Itoa(host_daemon_port))
-	service.Status = "running"
-	last_daemon_port = host_daemon_port
+	fmt.Println("Container started")
+	service := container_subscribe(containerName, imageName, strconv.Itoa(host_daemon_port))
+	service.Status = "Running"
 
 	return nil
 }
-
 
 func isPortInUse(port string) bool {
 	listener, err := net.Listen("tcp", ":"+port)
--- main.go
+++ main.go
@@ -2,20 +2,19 @@ package main
 
 import (
 	"fmt"
+
 	"github.com/gin-gonic/gin"
 )
 
-
 func main() {
-   r := gin.Default()
-   // define the routes
-   r.POST("/cm_run", run_handler)
-   r.POST("/cm_checkpoint", checkpoint_handler)
-   r.POST("/cm_subscribe",subscribe_handler)
-   r.POST("/cm_start", start_handler)
-   err := r.Run(":8787")
-   if err != nil {
-      fmt.Println("impossible to start server: %s", err)
-   }
+	r := gin.Default()
+	// define the routes
+	r.POST("/cm_run", run_handler)
+	r.POST("/cm_checkpoint", checkpoint_handler)
+	r.POST("/cm_subscribe", subscribe_handler)
+	r.POST("/cm_start", start_handler)
+	err := r.Run(":8787")
+	if err != nil {
+		fmt.Printf("impossible to start server: %s\n", err)
+	}
 }
-
